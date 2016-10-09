98380f537f787be8e20cf225038a9ecc05a1dfc1
Elbert Noel Nyunting <noel5550@users.noreply.github.com>
2016-10-09 22:31:10 +0200

    Made ActionBlock less of a mess

--- Actions/ActionBlock.cpp
+++ Actions/ActionBlock.cpp
@@ -6,6 +6,7 @@
  * @brief Fichier source définissant l'action de blocker un joueur adverse (et tout ce qui peut y arriver)
 */
 
+#include <vector>
 #include "ActionBlock.hpp"
 
 using namespace std;
@@ -42,46 +43,26 @@ Player ActionBlock::getActingPlayer(){
  * @param oppoPlayer, le joueur victime d'une tentative de blockage
  */
 void ActionBlock::doAction(Player oppoPlayer){
-	int advantage = 1;
+	int advantage;
 	int diceRes;
 	int diceChoice;
 	int finalRes;
-	bool inControl = false;
+	bool inControl = true;
 	vector<int> diceStack;
 	Dice d6(1, 6);
 	
-	
-	
-	if(actingPlayer.getStr() > oppoPlayer.getStr()){
-		++advantage;
-		if(actingPlayer.getStr >= oppoPlayer.getStr() * 2 + 1){
-			++advantage
-		}
-	}else if(actingPlayer.getStr() > oppoPlayer.getStr()){
-		--advantage;
-		if(actingPlayer.getStr <= oppoPlayer.getStr() * 2 + 1){
-			--advantage
-		}
-	}
-	 
-	if(actingPlayer_.outnumbered()){
-		--advantage;
-	}
-	
-	if(oppoPlayer.outnumbered()){
-		++advantage;
-	}
-	
-	if(advantage < 0){
+	advantage = actingPlayer_.countAdvantage(Player oppoPlayer);
+	if (advantage < 0){
 		advantage *= -1;
 		inControl = false;
 	}
 	
-	if(advantage > 3){
-		advantage = 3;
-	}
 	
 	std::cout << "Vous allez lancer " <<advantage<< " dés!" <<std::endl;
+	if(!inControl){
+		std::cout << "Mais le pire résultat sera pris!" <<std::endl;
+	}
+	
 	
 	for(int i = 0; i <= advantage; ++i){
 		diceRes = d6.throwDiceSingle()
@@ -103,11 +84,20 @@ void ActionBlock::doAction(Player oppoPlayer){
 	}
 	
 	if(diceStack.size() > 1){
-		std::cout << "Choisissez quel résultat vous allez choisir!" <<std::endl;
-		std::cin >> diceChoice;
-		if(diceChoice>diceStack.size() || diceChoice>3){
-			std::cout << "OY! Ca triche pas içi!" <<std::endl;
-		}
+		if(inControl){
+			std::cout << "Choisissez quel résultat vous allez choisir!" <<std::endl;
+			std::cin >> diceChoice;
+			if(diceChoice>diceStack.size() || diceChoice>3){
+				std::cout << "OY! Ca triche pas içi!" <<std::endl;
+			}
+		}else{
+			diceChoice = 6;
+			for(int i = 0; i < diceStack.size(); ++i){
+				if(diceChoice > diceStack.at(i)){
+					diceChoice = diceStack.at(i);
+				}
+			}
+		}	
 	}else{
 		diceChoice = 1;
 	}
@@ -117,45 +107,16 @@ void ActionBlock::doAction(Player oppoPlayer){
 	if(finalRes == 1){ //failed block, attacker down
 		actingPlayer_.downed();
 		std::cout << "Et voila! Il est à terre pour tenter un blockage! Comme quoi il ne faut pas embaucher des amateurs!" <<std::endl
-	}else if(finalRes == 2){ //both down, unless the guy with Block skill
-		if(actingPlayer_.block() && oppoPlayer.block()){ //none down
-			std::cout << "Les deux joueurs se cognent et rien ne se passe!" <<std::endl;
-		}else if(actingPlayer_.block()){ //opposing player down
-			oppoPlayer.downed();
-			std::cout << "BOOM! Ils se cognent mais un est nettement plus solide que l'autre!" <<std::endl
-		}else if(oppoPlayer_.block()){//acting player down et turnover
-			actingPlayer_.downed();
-			std::cout << "BOOM! Ils se cognent mais un est nettement plus solide que l'autre!" <<std::endl
-			actingPlayer_.turnover();
-		}else{ //both down, turnover
-			actingPlayer_.downed();
-			oppoPlayer.downed();
-			std::cout << "HA! Ils se cognent et sont tous les deux à terre!" <<std::endl
-			actingPlayer_.turnover();
-		}
+	
+	}else if(finalRes == 2){ 
+		actingPlayer_.bothDown(Player oppoPlayer);		
+	
 	}else if(finalRes == 3 || finalRes == 4){ //pushed
-		int spot = oppoPlayer.freeSpot();
-		if(spot>0){		//if there's space, pushed to a random space. Yes, it's not REALLY Blood Bowl, but cmon, man
-			oppoPlayer.pushBack();
-			std::cout << "Et il s'est fait poussé!" <<std::endl;
-		}else{ //if there's no space, attacking player downed
-			oppoPlayer.downed();
-			std::cout << "Manque de place, il s'est fait terrassé" <<std::endl;
-		}
-	}else if(finalRes == 5){ //knocked down with possibility to dodge, in which case it becomes pushed
-		if(oppoPlayer.Dodge()){
-			int spot = oppoPlayer.freeSpot();
-			if(spot>0){		
-				oppoPlayer.pushBack();
-				std::cout << "Et il a evadé le coup!" <<std::endl
-			}else{
-				oppoPlayer.downed();
-				std::cout << "Manque de place, il s'est fait terrassé" <<std::endl;
-			}
-		}else{
-			oppoPlayer.downed()
-			std::cout << "OUCH! Ca doit faire mal, ça!" <<std::endl;
-		}
+		actingPlayer_.pushed(Player oppoPlayer);	
+		
+	}else if(finalRes == 5){ 
+		actingPlayer_.stumble(Player oppoPlayer);
+		
 	}else(finalRes == 6){ //knocked down, no evading this bad boy
 		oppoPlayer.downed()
 		std::cout << "OUCH! Ca doit faire mal, ça!" <<std::endl;
@@ -163,4 +124,92 @@ void ActionBlock::doAction(Player oppoPlayer){
 	
 }
 
+/**
+ * @brief méthode retournant l'avantage de actifPlayer_ par rapport à oppoPlayer
+ * @return le nombre d'avantage actifPlayer_ a
+ * @detail advantage est le nombre de dés utilisés, donc pas plus que 3
+ */
+
+int ActionBlock::countAdvantage(Player oppoPlayer){
+	int advantage = 1;
+	if(actingPlayer_.getStr() > oppoPlayer.getStr()){
+		++advantage;
+		if(actingPlayer_.getStr >= oppoPlayer.getStr() * 2 + 1){
+			++advantage
+		}
+	}else if(actingPlayer_.getStr() < oppoPlayer.getStr()){
+		--advantage;
+		if(actingPlayer_.getStr <= oppoPlayer.getStr() * 2 + 1){
+			--advantage
+		}
+	}
+	 
+	if(actingPlayer_.outnumbered()){
+		--advantage;
+	}
+	
+	if(oppoPlayer.outnumbered()){
+		++advantage;
+	}
+	
+	if(advantage < -3){
+		advantage = -3;
+	}else if(advantage > 3){
+		advantage = 3;
+	}
+	
+	return advantage;
+}
+
+/**
+ * @brief méthode simulant l'effet de deux joueurs se tapant desus
+ * @detail le(s) joueur(s) qui a le skill Block ne sera pas à terre
+ */
+void ActionBlock::bothDown(Player oppoPlayer){
+	//both down, unless the guy with Block skill
+	if(actingPlayer_.block() && oppoPlayer.block()){ //none down
+		std::cout << "Les deux joueurs se cognent et rien ne se passe!" <<std::endl;
+	}else if(actingPlayer_.block()){ //opposing player down
+		oppoPlayer.downed();
+		std::cout << "BOOM! Ils se cognent mais un est nettement plus solide que l'autre!" <<std::endl
+	}else if(oppoPlayer_.block()){//acting player down et turnover
+		actingPlayer_.downed();
+		std::cout << "BOOM! Ils se cognent mais un est nettement plus solide que l'autre!" <<std::endl
+		actingPlayer_.turnover();
+	}else{ //both down, turnover
+		actingPlayer_.downed();
+		oppoPlayer.downed();
+		std::cout << "HA! Ils se cognent et sont tous les deux à terre!" <<std::endl
+		actingPlayer_.turnover();
+	}
+}
+
+/**
+ * @brief méthode simulant l'effet d'un joueur qui est poussé
+ * @detail oppoPlayer sera pausé vers une case adjacente aléatoire
+ * @detail Si pas de case adjacente libre, oppoPlayer sera assomé
+ */
+void ActionBlock::pushed(Player oppoPlayer){
+	int spot = oppoPlayer.freeSpot();
+	if(spot>0){		//if there's space, pushed to a random space. Yes, it's not REALLY Blood Bowl, but cmon, man
+		oppoPlayer.pushBack();
+		std::cout << "Et il s'est fait poussé!" <<std::endl;
+	}else{ //if there's no space, attacking player downed
+		oppoPlayer.downed();
+		std::cout << "Manque de place, il s'est fait terrassé" <<std::endl;
+	}
+}
+
+/**
+ * @brief méthode simulant l'effet d'un joueur terrassé
+ * @detail si oppoPlayer a le skill Dodge, il ne sera que poussé (avec les effets qui vont avec)
+ */
+void ActionBlock::stumble(Player oppoPlayer){
+	if(oppoPlayer.Dodge()){
+		actingPlyaer_.pushed(Player oppoPlayer)
+	}else{
+		oppoPlayer.downed()
+		std::cout << "OUCH! Ca doit faire mal, ça!" <<std::endl;
+	}
+}
 
