473a32ffe745a529976d121e5b4ae37b48ace146
noel5550 <noel.nyunting@gmail.com>
2016-10-09 21:59:19 +0200

    Add ActionPass.hpp and modified the cpp file

---
+++ Actions/ActionPass.hpp
@@ -0,0 +1,31 @@
+#ifndef __ACTIONPASS_HPP__
+#define __ACTIONPASS_HPP__
+/**
+ * @file ActionPass.hpp
+ * @copyright WTFPL v2
+ * @author Elbert NYUNTING
+ * @date 08/10/2016
+ * @brief Fichier hpp définissant l'action de passer et reçevoir la balle (ainsi que le fait for probable de les rater)
+*/
+#include <queue>
+#include <vector>
+
+
+class ActionPass : public Actions{
+    public:
+	ActionPass(Player actingPlayer, Game game);
+	~ActionBlock();
+	Player getActingPlayer();
+	Game getGame();
+	void setActingPlayer(Player actingPlayer);
+	void doAction(Player playerCible);
+	void passFail();
+	int rollPassDices();
+	void receiveAct(int modif);
+	
+    private:
+    	Player actingPlayer_;
+	Game game_;
+};
+
+#endif
