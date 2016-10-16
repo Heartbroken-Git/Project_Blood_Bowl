a12df74755dce7ad54efbc6b9b8aa9f2428e1fb9
Heartbroken-Git <corentin196@gmail.com>
2016-10-17 00:02:53 +0200

    added the header file for ActionDodge

---
+++ Actions/ActionDodge.hpp
@@ -0,0 +1,33 @@
+#ifndef __ACTIONDODGE__
+#define __ACTIONDODGE__
+/**
+ * @file ActionDodge.hpp
+ * @copyright WTFPL v2
+ * @author Corentin CHÉDOTAL
+ * @date 16/10/2016
+ * @brief Fichier entête de l'Action Dodge
+ */
+ 
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @class ActionDodge
+ * @brief Classe définissant l'action Dodge
+ * @details L'action Dodge est l'acte d'un joueur d'éviter une attaque ennemie, elle dépent principalement de l'agilité du joueur attaqué
+ */
+class ActionDodge : public Actions {
+	private:
+		Player actingPlayer_;
+	
+	public:
+		ActionDodge(Player);
+		~ActionDodge();
+		void doAction();
+		Player getActingPlayer();
+		
+};
+
+#endif
