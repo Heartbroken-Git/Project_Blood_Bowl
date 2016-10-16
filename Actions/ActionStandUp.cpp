9ad8cef170a1fbf1dd6929f958fb38d548c89461
Elbert Noel Nyunting <noel5550@users.noreply.github.com>
2016-10-16 12:19:54 +0200

    Add files via upload

---
+++ Actions/ActionStandUp.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file ActionStandUp.hpp
+ * @copyright WTFPL v2
+ * @author Elbert NYUNTING
+ * @date 16/10/2016
+ * @brief Fichier hpp définissant l'action d'un joueur se relève
+*/
+
+#include <vector>
+#include "ActionStandUp.hpp"
+
+using namespace std;
+
+/**
+ * @brief Constructeur de ActionStandUp
+ * @param actingPlayer le joueur actif
+ * @pre actingPlayer doit être un joueur de l'équipe jouant en jeu
+ */
+ActionStandUp::ActionStandUp(Player actingPlayer){
+	actingPlayer_ = actingPlayer;
+}
+
+/**
+ * @brief Destructeur de ActionStandUp
+ */
+ActionStandUp::~ActionStandUp(){}
+
+/**
+ * @brief méthode retournant le joueur actif
+ * @return le joueur actif
+ */
+Player ActionStandUp::getActingPlayer(){
+	return actingPlayer_;
+}
+
+/**
+ * @brief méthode qui fait relever un joueur qui est à terre
+ * @details le status_ d'un Player est un integer tel que
+ * @details 0 == debout, 1 == debout, -3 mouvement & !tackle, 2 == au sol, 3 == sur le ventre, 4 == KO, 5 == Mort, 6 == en reserve
+ */
+void ActionStandUp::doAction(){
+	if (actingPlayer_.getStatus() == 2){
+		actingPlayer_.setStatus(1));
+		std::cout<<actingPlayer_.getName()<<" s'est relevé!"<<std::endl;
+	}else{
+		std::cout<<"Ce joueur n'est pas au sol!"<<std::endl;
+		return;
+	}
+}
---
+++ Actions/ActionStandUp.hpp
@@ -0,0 +1,28 @@
+#ifndef __ACTIONSTANDUP_HPP__
+#define __ACTIONSTANDUP_HPP__
+/**
+ * @file ActionStandUp.hpp
+ * @copyright WTFPL v2
+ * @author Elbert NYUNTING
+ * @date 16/10/2016
+ * @brief Fichier hpp définissant l'action d'un joueur se relève
+*/
+
+#include <queue>
+#include <vector>
+
+
+class ActionStandUp : public Actions{
+    public:
+	ActionStandUp();
+	~ActionStandUp();
+	void doAction(Player player);
+	Player getActingPlayer();
+	
+	
+    private:
+	Player actingPlayer_;
+};
+
+#endif
+
