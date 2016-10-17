9d64fcba200c06019fe41f4b1527391eeccd34bf
Elbert Noel Nyunting <noel5550@users.noreply.github.com>
2016-10-17 16:14:34 +0200

    Update ActionGoForIt.hpp

--- Actions/ActionGoForIt.hpp
+++ Actions/ActionGoForIt.hpp
@@ -21,7 +21,7 @@ using namespace std;
  * @brief Classe définissant l'action Go For It
  * @details L'action Go For It permet à un joueur de tenter de pousser son joueur à aller plus loin que son déplacement classique au risque de glisser le faisant tomber à terre et mettant fin au tour en cours
  */
-class ActionGoForIt : public Actions {
+class ActionGoForIt : public virtual Actions {
 	private:
 		Player actingPlayer_;
 	
