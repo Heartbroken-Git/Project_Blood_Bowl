d2a6f1e66ca51b45b8d9ff4ad38b7777b821b25a
Heartbroken-Git <corentin196@gmail.com>
2016-10-17 07:15:49 +0200

    initial addition of ActionDodge.cpp (to be checked against the actual rules)

---
+++ Actions/ActionDodge.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file ActionDodge.cpp
+ * @copyright WTFPL v2
+ * @author Corentin CHÉDOTAL
+ * @date 16/10/2016
+ * @brief Fichier source de l'Action Dodge
+ */
+ 
+#include "ActionDodge.hpp"
+
+/**
+ * @brief Constructeur de ActionDodge
+ * @param actingPlayer le joueur exécutant l'action
+ * @pre actingPlayer_ doit être un joueur effectivement sur le terrain
+ */
+ActionDodge::ActionDodge(Player actingPlayer) {
+	actingPlayer_ = actingPlayer;
+}
+
+/**
+ * @brief Destructeur de ActionGoForIt
+ */
+ActionDodge::~ActionDodge() {}
+
+/**
+ * @brief Méthode exécutant l'action Dodge
+ * @details Lance le dé appliquant un modificateur lié au nombre de joueurs ennemis présent autour du joueur, la réussite dépend donc du modificateur et de l'agilité du joueur attaqué.
+ * @details Si le test échoue le joueur doit alors tenter de bloquer l'attaque (renvoie sur ActionBlock).
+ */
+void ActionDodge::doAction() {
+	int modifier = 1 + (0 - actingPlayer_.oppoAjac());
+	Dice d6(1, 6);
+	if ((d6.throwDiceSingle() + modifier) >= 7 - actingPlayer_.getAG()) {
+		//DODGED
+	} else {
+		ActionBlock tentativeBloquage(actingPlayer_);
+		tentativeBloquage.doAction();
+	}
+}
+
+/**
+ * @brief Accesseur de actingPlayer_
+ * @return Retourne le joueur agissant
+ */
+Player ActionDodge::getActingPlayer() {
+	return(actingPlayer_);
+}
