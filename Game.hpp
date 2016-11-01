fd28c4348494131151c9ee5748b9d833eecccf77
Elbert Noel Nyunting <noel5550@users.noreply.github.com>
2016-11-01 10:18:24 +0100

    Created Game.hpp
    Need to be put in a Game folder, change the #include so it can reach Ball

---
+++ Game.hpp
@@ -0,0 +1,23 @@
+#ifndef __GAME_HPP__
+#define __GAME_HPP__
+
+/*!
+ * @file Game.hpp
+ * @author Elbert NYUNTING
+ * @brief Protoype Game
+ */
+ 
+ #include "Ball/Ball.hpp"
+ 
+ using namespace std;
+ 
+ class Game{
+       public:
+              Game();
+              ~Game();
+              Ball getBall();
+              
+       private:
+             Ball ball_; 
+ 
+ }
