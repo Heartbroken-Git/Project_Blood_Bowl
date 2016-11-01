7c23a5a015ec508396d7d1a8dd224fd2cb722e88
Elbert Noel Nyunting <noel5550@users.noreply.github.com>
2016-11-01 10:21:59 +0100

    Added Game.cpp
    Fichier bateau, à mettre dans un dossier Game

---
+++ Game.cpp
@@ -0,0 +1,21 @@
+/*!
+ * @file Game.cpp
+ * @author Elbert NYUNTING
+ * @brief Protoype Game
+ */
+
+#include "Game.hpp"
+using namespace std;
+
+Game::Game(Ball ball){
+    ball_ = ball;
+}
+
+Game::~Game(){
+    std::cout<<"Game ended."<<std::endl;
+}
+
+Ball Game::getBall(){
+  return ball_;
+}
+
