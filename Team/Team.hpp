b4f40aaaeb6f2b1f1c7dd69bfe051674ef8e47b3
Heartbroken-Git <corentin196@gmail.com>
2016-11-24 16:52:55 +0000

    remade Tile.hpp to the UML standard

--- Team/Team.hpp
+++ Team/Team.hpp
@@ -1,33 +1,43 @@
 #ifndef __Team_HPP__
 #define __Team_HPP__
 
-/*!
+/**
  * @file Team.hpp
- * @author Elbert NYUNTING
- * @brief Protoype
+ * @copyright WTFPL v2
+ * @author Corentin CHÉDOTAL
+ * @date 24/11/2016
+ * @brief Fichier entête des Team
  */
  
 #include <iostream>
 #include <vector>
+#include <string>
 
+/**
+ * @class Team
+ * @brief Classe abstraite définissant les équipes du jeu
+ * @details Les équipes d'un coach contiendront des listes de joueurs inclus dans l'équipe, le nom de l'équipe, ainsi que la Race de l'équipe.
+ */
 class Team{
+private:
+	int nbScore_;
+	string nom_;
+	Race teamRace_;
+	vector<Player> roster_;
+
 public:
-	int i;
-	vector<Player> roster;
-	vector<Player>::iterator iter;
 	Team();
-	int wins;
-	int losses;
-	int draws;
-	void setWins(int);
-	void setLosses(int);
-	void setDraws(int);
-	int getWins();
-	int getLosses();
-	int getDraws();
+	~Team();
+	int getNbScore();
+	int getNom();
+	Race getRace();
+	Player getPlayer(int);
+	void setNbScore(int);
+	void setNom(string);
+	void setRace(Race);
+	void setPlayer(int, Player);
 	void addPlayer(Player);
 	void removePlayer(int);
-	int teamSize();
 };
  
  
