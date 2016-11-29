1b5912dea7a2368efb8240cc85884cabca04c01e
Elbert Noel Nyunting <noel5550@users.noreply.github.com>
2016-11-30 00:01:24 +0100

    Update Ball.hpp

--- Ball/Ball.hpp
+++ Ball/Ball.hpp
@@ -3,11 +3,11 @@
 /**
  * @file Ball.hpp
  * @author Elbert NYUNTING 
- * @brief Class Ball for Blood Bowl, fickle litthe thing...
+ * @brief Class Ball for Blood Bowl, fickle litle thing...
 **/
 
-#include <queue>
-#include <vector>
+#include <memory>
+#include <iostream>
 #include "../Tile/Tile.hpp"
 #include "../Player/Player.hpp"
 
@@ -18,16 +18,18 @@
 **/
 class Ball{
     public:
-    Ball();
+    Ball(int x, int y, Grid grid);
     ~Ball();
     Player getHolder();
     int getX();
     int getY();
     void setHolder(Player holder);
+    bool isHeldByPlayer();
     void bounce();
+   
     
     private:
-    Player holder_;
+    std::shared_ptr<Player> holder_;
     int x_;
     int y_;
     Grid grid_;
