3799e66ffc5d61affbebcd49668914fcb70c00de
Heartbroken-Git <corentin196@gmail.com>
2016-12-08 01:11:35 +0100

    started to work on Game, too tired to continue, tomorrow will go back to it

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,23 +1,42 @@
-/*!
- * @file Game.cpp
+/**
+ * @file Game.hpp
+ * @copyright WTFPL v2
+ * @author Corentin CHÉDOTAL
  * @author Elbert NYUNTING
- * @brief Protoype Game
+ * @date 08/12/2016
+ * @brief Fichier source de la classe décrivant la partie en cours
  */
 
 #include "Game.hpp"
+
 using namespace std;
 
-Game::Game(Ball ball){
+/**
+ * @brief Constructeur de Game
+ * @param ball la Ball qui sera utilisée durant la partie
+ * @param t1 la Team choisie par le joueur 1
+ * @param t2 la Team choisie par le joueur 2
+ */
+Game::Game(Ball ball, Team t1, Team t2){
     ball_ = ball;
+    team1_ = t1;
+    team2_ = t2;
 }
 
-Game::Game(){}
-
+/**
+ * @brief Destructeur de Game
+ */
 Game::~Game(){
     std::cout<<"Game ended."<<std::endl;
 }
 
+/**
+ * @brief Accesseur à la Ball de la partie
+ * @return la Ball utilisée par la Game en cours
+ */
 Ball Game::getBall(){
   return ball_;
 }
 
+int tileDist(Player player1, Player player2) {
+	
