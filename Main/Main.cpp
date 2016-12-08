0e1549327e5d8c92ffd8fc144217b6c9c3daa916
Heartbroken-Git <corentin196@gmail.com>
2016-12-08 10:46:41 +0100

    first attempt at having a true Main

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -7,13 +7,31 @@
 #include <iostream>
 #include <vector>
 
-#include "../Dice/Dice.hpp"
+#include "../Ball/Ball.hpp"
+#include "../Game/Game.hpp"
+#include "../Team/Team.hpp"
+#include "../Team/TeamCompoViolent.hpp"
+#include "../Team/TeamCompoAgile.hpp"
+#include "../Commands/Commands.hpp"
 
 using namespace std;
 
 int main(){
-	Dice d6(1,6);
-	int res;
-	res = d6.throwDiceSingle();
-	std::cout<<res<<std::endl;
+	Ball ball;
+	TeamCompoViolent compoOrc;
+	TeamCompoAgile compoElf;
+	
+	Team team1 = compoOrc.createTeam();
+	Team team2 = compoElf.createTeam();
+	
+	
+	Game game(ball, team1, team2);
+	
+	Commands cmd(game);
+	
+	while(true) {
+		cmd.input();
+	}
+	
+	return(0);
 }
